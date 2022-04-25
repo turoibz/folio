18691493f2de1ed9dd1d1760641ffb878ef1fd74
Arturo Ibanez <turoibz@gmail.com>
2022-04-25 23:11:30 +1000

    Update App.js

--- src/app/App.js
+++ src/app/App.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { StyledApp, StyledContentWrapper } from "./App.styled";
 import { Header } from "../components/Header/Header";
 import { Footer } from "../components/Footer/Footer";
 
 const App = ({ children }) => {
+
+  const [hasMounted, setHasMounted] = useState(false);
+
+  useEffect(() => {
+    setHasMounted(true);
+  }, []);
+  
+  if (!hasMounted) {
+    return null;
+  }
+
   return (
     <StyledApp>
       <Header/>
